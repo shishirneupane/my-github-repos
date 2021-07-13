1d1821b1639677223fdc8bdd474ee32735f67886
Shishir <shishirs520@gmail.com>
2021-07-14 01:06:03 +0545

    [create] created separate files for CSS and JS

---
+++ script.js
@@ -0,0 +1,30 @@
+fetch('https://api.github.com/users/shishirneupane')
+.then((res) => res.json())
+.then((data) => {
+	console.log(data)
+	document.getElementById('profileImage').src = data['avatar_url']
+	document.getElementById('fullName').textContent = data['name']
+	document.getElementById('bio').textContent = data['bio']
+	document.getElementById('followersInfo').textContent = 'GitHub followers: ' + data['followers']
+	document.getElementById('followingInfo').textContent = 'GitHub following: ' + data['following']
+	document.getElementById('reposInfo').textContent = 'Public repositories: ' + data['public_repos']
+	document.getElementById('githubLink').href = data['html_url']
+	document.getElementById('linkedinLink').href = data['blog']
+	document.getElementById('mainContainer').hidden = false
+	document.getElementById('loading').hidden = true
+})
+.catch((err) => console.log(err));
+
+
+fetch('https://api.github.com/users/shishirneupane/repos')
+.then(res => res.json())
+.then((reposData) => {
+	console.log(reposData)
+	let output = '<h2>My Projects</h2>'
+	document.getElementById('output').innerHTML = output;
+	for (var i=0; i < reposData.length; i++) {
+		document.getElementById('output').innerHTML += 
+		"<a href=" + reposData[i].html_url + "/>" + reposData[i].name + "<br/><br/>";
+	}
+})
+.catch((err) => console.log(err));
